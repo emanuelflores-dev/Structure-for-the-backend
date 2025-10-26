2bcbf876fedb47ce6cf00930df1603b780218627
Emanuel Flores Aroca <emanuel.flores.dev@gmail.com>
2025-10-26 19:49:14 -0300

    Implement CRUD methods in ProfesoresController for database interactions

--- controller/profes.controller.js
+++ controller/profes.controller.js
@@ -4,16 +4,50 @@ class ProfesoresController {
   constructor() {}
 
   consultar(req, res) {
-    res.json({ message: "Consulta profesores" });
+    try {
+      db.query(`SELECT * FROM profesores;`, (err, rows) => {
+        if (err) {
+          return res.status(400).send(err);
+        }
+        res.status(200).json(rows);
+      });
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
   }
 
   consultarDetalle(req, res) {
     const { id } = req.params;
-    res.json({ message: `Se consulto al Profesor con ID: ${id}` });
+    try {
+      db.query(`SELECT * FROM profesores WHERE id = ?`, [id], (err, row) => {
+        if (err) {
+          return res.status(400).send(err);
+        }
+        res.status(200).json(row);
+      });
+    } catch (error) {
+      res.status(500).json(error.message);
+    }
   }
 
   ingresar(req, res) {
-    res.json({ message: "Ingresar Profesor" });
+    try {
+      const { dni, nombre, apellido, profesion, email } = req.body;
+      db.query(
+        `INSERT INTO profesores
+          (id, dni, nombre, apellido, profesion, email)
+          VALUES (NULL, ?, ?, ?, ?, ?);`,
+        [dni, nombre, apellido, profesion, email],
+        (err, rows) => {
+          if (err) {
+            return res.status(400).send(err.message);
+          }
+          res.status(201).json({ id: rows.insertId });
+        }
+      );
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
   }
 
   actualizar(req, res) {
